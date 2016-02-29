6513eaaca717fc5b47f1aac654678891890bc562
Kevin Fisher <kevinfisher@kevins-mbp.attlocal.net>
2016-02-29 00:09:33 -0600

    2/29

--- src/fitnesstracker/gui/TrackAWorkout.java
+++ src/fitnesstracker/gui/TrackAWorkout.java
@@ -41,6 +41,7 @@ public class TrackAWorkout {
 
 		frame.getContentPane().removeAll();
 		
+		//create an options array for the joptionpane box that pops up
 		Object[] options = 
 			{
 				"Workout 1",
@@ -48,7 +49,9 @@ public class TrackAWorkout {
 				"Workout 3",
 				"Workout 4"
 			};
-		String input = (String)JOptionPane.showInputDialog(
+		
+		//create and activate a joptionpane asking which workout the user would like to track
+		final String input = (String)JOptionPane.showInputDialog(
 								frame,
 								"Which workout would you like to track?",
 								"Choose workout",
@@ -56,23 +59,6 @@ public class TrackAWorkout {
 								null,
 								options,
 								options[0]);
-		
-		//make panel and set layout
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new GridLayout(16,0));					
-							
-		//date label and text field
-		JLabel dateLabel = new JLabel();
-		dateLabel.setLayout(new FlowLayout(FlowLayout.LEFT));
-		
-		JLabel dateText = new JLabel();
-		dateText.setText("Date (MM/DD/YY)");
-		dateLabel.add(dateText);
-		
-		final JTextField dateInput = new JTextField(10);
-		//dateInput.getDocument().putProperty("dateInput" , dateInput);
-		dateLabel.add(dateInput);
-		mainPanel.add(dateLabel);
 		
 		//weight jtextfields
 		final JTextField mainLift1WeightInput = new JTextField(4),
@@ -106,6 +92,8 @@ public class TrackAWorkout {
 							accessory2Set1Input = new JTextField(2),
 							accessory3Set1Input = new JTextField(2);
 		
+		//each of the text elemets on the page are put in a jlabel
+		//so create the jlabels here, and set the text for the "mainLift#Text" later depending on what workout the user chose
 		final JLabel mainLift1Text = new JLabel(), 
 				mainLift1WeightText = new JLabel(), 
 				mainLift1Set1Text = new JLabel(), 
@@ -148,6 +136,7 @@ public class TrackAWorkout {
 				accessory3WeightText = new JLabel(),
 				accessory3Set1Text = new JLabel();
 		
+		//set the text for the weight and set jlabels, since they will be the same for each workout
 		mainLift1WeightText.setText("Weight");
 		mainLift2WeightText.setText("Weight");
 		mainLift3WeightText.setText("Weight");
@@ -181,37 +170,137 @@ public class TrackAWorkout {
 		accessory2Set1Text.setText("Set 1");
 		accessory3Set1Text.setText("Set 1");
 		
-		if (input.equals("Workout 1")) {
-			workoutNumber = 1;
-			
+		
+		//make panel and set layout
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new GridLayout(16,0));					
+							
+		//date label and text field
+		JLabel dateLabel = new JLabel();
+		dateLabel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		JLabel dateText = new JLabel();
+		dateText.setText("Date (MM/DD/YY)");
+		dateLabel.add(dateText);
+		
+		final JTextField dateInput = new JTextField(10);
+		//dateInput.getDocument().putProperty("dateInput" , dateInput);
+		dateLabel.add(dateInput);
+		mainPanel.add(dateLabel);
+		
+		
+		//create labels to hold the fields for each lift
+		
+		JLabel mainLift1Fields = new JLabel();
+		mainLift1Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		mainLift1Fields.add(mainLift1WeightText);
+		//squatWeightInput.getDocument().putProperty("squatWeightInput" , squatWeightInput);
+		mainLift1Fields.add(mainLift1WeightInput);
+		
+		mainLift1Fields.add(mainLift1Set1Text);
+		mainLift1Fields.add(mainLift1Set1Input);
+		
+		mainLift1Fields.add(mainLift1Set2Text);
+		mainLift1Fields.add(mainLift1Set2Input);
+		
+		mainLift1Fields.add(mainLift1Set3Text);
+		mainLift1Fields.add(mainLift1Set3Input);
+		
+		mainLift1Fields.add(mainLift1Set4Text);
+		mainLift1Fields.add(mainLift1Set4Input);
+
+		mainLift1Fields.add(mainLift1Set5Text);
+		mainLift1Fields.add(mainLift1Set5Input);
+
+		mainLift1Fields.add(mainLift1Set6Text);
+		mainLift1Fields.add(mainLift1Set6Input);
+		
+		JLabel mainLift2Fields = new JLabel();
+		mainLift2Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		mainLift2Fields.add(mainLift2WeightText);
+		mainLift2Fields.add(mainLift2WeightInput);
+		
+		mainLift2Fields.add(mainLift2Set1Text);
+		mainLift2Fields.add(mainLift2Set1Input);
+		
+		mainLift2Fields.add(mainLift2Set2Text);
+		mainLift2Fields.add(mainLift2Set2Input);
+
+		mainLift2Fields.add(mainLift2Set3Text);
+		mainLift2Fields.add(mainLift2Set3Input);
+
+		mainLift2Fields.add(mainLift2Set4Text);
+		mainLift2Fields.add(mainLift2Set4Input);
+		
+		JLabel mainLift3Fields = new JLabel();
+		mainLift3Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		mainLift3Fields.add(mainLift3WeightText);
+		mainLift3Fields.add(mainLift3WeightInput);
+		
+		mainLift3Fields.add(mainLift3Set1Text);
+		mainLift3Fields.add(mainLift3Set1Input);
+		
+		mainLift3Fields.add(mainLift3Set2Text);
+		mainLift3Fields.add(mainLift3Set2Input);
+		
+		mainLift3Fields.add(mainLift3Set3Text);
+		mainLift3Fields.add(mainLift3Set3Input);
+		
+		mainLift3Fields.add(mainLift3Set4Text);
+		mainLift3Fields.add(mainLift3Set4Input);
+		
+		JLabel mainLift4Fields = new JLabel();
+		mainLift4Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		mainLift4Fields.add(mainLift4WeightText);
+		mainLift4Fields.add(mainLift4WeightInput);
+		
+		mainLift4Fields.add(mainLift4Set1Text);
+		mainLift4Fields.add(mainLift4Set1Input);
+		
+		mainLift4Fields.add(mainLift4Set2Text);
+		mainLift4Fields.add(mainLift4Set2Input);
+		
+		mainLift4Fields.add(mainLift4Set3Text);
+		mainLift4Fields.add(mainLift4Set3Input);
+		
+		mainLift4Fields.add(mainLift4Set4Text);
+		mainLift4Fields.add(mainLift4Set4Input);
+		
+		JLabel accessory1Fields = new JLabel();
+		accessory1Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		accessory1Fields.add(accessory1WeightText);
+		accessory1Fields.add(accessory1WeightInput);
+
+		accessory1Fields.add(accessory1Set1Text);
+		accessory1Fields.add(accessory1Set1Input);
+		
+		JLabel accessory2Fields = new JLabel();
+		accessory2Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		accessory2Fields.add(accessory2WeightText);
+		accessory2Fields.add(accessory2WeightInput);
+
+		accessory2Fields.add(accessory2Set1Text);
+		accessory2Fields.add(accessory2Set1Input);
+		
+		JLabel accessory3Fields = new JLabel();
+		accessory3Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		accessory3Fields.add(accessory3WeightText);
+		accessory3Fields.add(accessory3WeightInput);
+
+		accessory3Fields.add(accessory3Set1Text);
+		accessory3Fields.add(accessory3Set1Input);
+		
+		if (input.equals("Workout 1")) {			
 			//main lift 1
 			mainLift1Text.setText("Squat");
 			mainPanel.add(mainLift1Text);
-			
-			JLabel mainLift1Fields = new JLabel();
-			mainLift1Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-			
-			mainLift1Fields.add(mainLift1WeightText);
-			//squatWeightInput.getDocument().putProperty("squatWeightInput" , squatWeightInput);
-			mainLift1Fields.add(mainLift1WeightInput);
-			
-			mainLift1Fields.add(mainLift1Set1Text);
-			mainLift1Fields.add(mainLift1Set1Input);
-			
-			mainLift1Fields.add(mainLift1Set2Text);
-			mainLift1Fields.add(mainLift1Set2Input);
-			
-			mainLift1Fields.add(mainLift1Set3Text);
-			mainLift1Fields.add(mainLift1Set3Input);
-			
-			mainLift1Fields.add(mainLift1Set4Text);
-			mainLift1Fields.add(mainLift1Set4Input);
-
-			mainLift1Fields.add(mainLift1Set5Text);
-			mainLift1Fields.add(mainLift1Set5Input);
-
-			mainLift1Fields.add(mainLift1Set6Text);
-			mainLift1Fields.add(mainLift1Set6Input);
 			
 			mainPanel.add(mainLift1Fields);
 			
@@ -219,47 +308,11 @@ public class TrackAWorkout {
 			mainLift2Text.setText("Bench Press");
 			mainPanel.add(mainLift2Text);
 			
-			JLabel mainLift2Fields = new JLabel();
-			mainLift2Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-			
-			mainLift2Fields.add(mainLift2WeightText);
-			mainLift2Fields.add(mainLift2WeightInput);
-			
-			mainLift2Fields.add(mainLift2Set1Text);
-			mainLift2Fields.add(mainLift2Set1Input);
-			
-			mainLift2Fields.add(mainLift2Set2Text);
-			mainLift2Fields.add(mainLift2Set2Input);
-
-			mainLift2Fields.add(mainLift2Set3Text);
-			mainLift2Fields.add(mainLift2Set3Input);
-
-			mainLift2Fields.add(mainLift2Set4Text);
-			mainLift2Fields.add(mainLift2Set4Input);
-			
 			mainPanel.add(mainLift2Fields);
 			
 			//main lift 3
 			mainLift3Text.setText("Deadlift");
 			mainPanel.add(mainLift3Text);
-			
-			JLabel mainLift3Fields = new JLabel();
-			mainLift3Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-			
-			mainLift3Fields.add(mainLift3WeightText);
-			mainLift3Fields.add(mainLift3WeightInput);
-			
-			mainLift3Fields.add(mainLift3Set1Text);
-			mainLift3Fields.add(mainLift3Set1Input);
-			
-			mainLift3Fields.add(mainLift3Set2Text);
-			mainLift3Fields.add(mainLift3Set2Input);
-			
-			mainLift3Fields.add(mainLift3Set3Text);
-			mainLift3Fields.add(mainLift3Set3Input);
-			
-			mainLift3Fields.add(mainLift3Set4Text);
-			mainLift3Fields.add(mainLift3Set4Input);
 			
 			mainPanel.add(mainLift3Fields);
 			
@@ -267,73 +320,156 @@ public class TrackAWorkout {
 			mainLift4Text.setText("Overhead Press");
 			mainPanel.add(mainLift4Text);
 			
-			JLabel mainLift4Fields = new JLabel();
-			mainLift4Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-			
-			mainLift4Fields.add(mainLift4WeightText);
-			mainLift4Fields.add(mainLift4WeightInput);
-			
-			mainLift4Fields.add(mainLift4Set1Text);
-			mainLift4Fields.add(mainLift4Set1Input);
-			
-			mainLift4Fields.add(mainLift4Set2Text);
-			mainLift4Fields.add(mainLift4Set2Input);
-			
-			mainLift4Fields.add(mainLift4Set3Text);
-			mainLift4Fields.add(mainLift4Set3Input);
-			
-			mainLift4Fields.add(mainLift4Set4Text);
-			mainLift4Fields.add(mainLift4Set4Input);
-			
 			mainPanel.add(mainLift4Fields);
 			
 			//accessory 1
-			accessory1Text.setText("Lat Pulldowns");
+			accessory1Text.setText("Lat Pulldown");
 			mainPanel.add(accessory1Text);
-			
-			JLabel accessory1Fields = new JLabel();
-			accessory1Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-			
-			accessory1Fields.add(accessory1WeightText);
-			accessory1Fields.add(accessory1WeightInput);
-
-			accessory1Fields.add(accessory1Set1Text);
-			accessory1Fields.add(accessory1Set1Input);
 			
 			mainPanel.add(accessory1Fields);
 			
 			//accessory 2
-			accessory2Text.setText("Rows");
+			accessory2Text.setText("Row");
 			mainPanel.add(accessory2Text);
-			
-			JLabel accessory2Fields = new JLabel();
-			accessory2Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-
-			accessory2Fields.add(accessory2WeightText);
-			accessory2Fields.add(accessory2WeightInput);
-
-			accessory2Fields.add(accessory2Set1Text);
-			accessory2Fields.add(accessory2Set1Input);
 			
 			mainPanel.add(accessory2Fields);
 			
 			//accessory 3
-			accessory3Text.setText("Bicep Curls");
+			accessory3Text.setText("Bicep Curl");
 			mainPanel.add(accessory3Text);
-			
-			JLabel accessory3Fields = new JLabel();
-			accessory3Fields.setLayout(new FlowLayout(FlowLayout.LEFT));
-
-			accessory3Fields.add(accessory3WeightText);
-			accessory3Fields.add(accessory3WeightInput);
-
-			accessory3Fields.add(accessory3Set1Text);
-			accessory3Fields.add(accessory3Set1Input);
 			
 			mainPanel.add(accessory3Fields);
 		}
-		
-		
+		else if (input.equals("Workout 2")) {
+			//main lift 1
+			mainLift1Text.setText("Overhead Press");
+			mainPanel.add(mainLift1Text);
+			
+			mainPanel.add(mainLift1Fields);
+			
+			//main lift 2
+			mainLift2Text.setText("Squat");
+			mainPanel.add(mainLift2Text);
+			
+			mainPanel.add(mainLift2Fields);
+			
+			//main lift 3
+			mainLift3Text.setText("Bench Press");
+			mainPanel.add(mainLift3Text);
+			
+			mainPanel.add(mainLift3Fields);
+			
+			//main lift 4
+			mainLift4Text.setText("Deadlift");
+			mainPanel.add(mainLift4Text);
+			
+			mainPanel.add(mainLift4Fields);
+			
+			//accessory 1
+			accessory1Text.setText("45 Degree Back Raise");
+			mainPanel.add(accessory1Text);
+			
+			mainPanel.add(accessory1Fields);
+			
+			//accessory 2
+			accessory2Text.setText("Leg Extension");
+			mainPanel.add(accessory2Text);
+			
+			mainPanel.add(accessory2Fields);
+			
+			//accessory 3
+			accessory3Text.setText("Leg Curl");
+			mainPanel.add(accessory3Text);
+			
+			mainPanel.add(accessory3Fields);
+		}
+		else if (input.equals("Workout 3")) {
+			//main lift 1
+			mainLift1Text.setText("Deadlift");
+			mainPanel.add(mainLift1Text);
+			
+			mainPanel.add(mainLift1Fields);
+			
+			//main lift 2
+			mainLift2Text.setText("Overhead Press");
+			mainPanel.add(mainLift2Text);
+			
+			mainPanel.add(mainLift2Fields);
+			
+			//main lift 3
+			mainLift3Text.setText("Squat");
+			mainPanel.add(mainLift3Text);
+			
+			mainPanel.add(mainLift3Fields);
+			
+			//main lift 4
+			mainLift4Text.setText("Bench Press");
+			mainPanel.add(mainLift4Text);
+			
+			mainPanel.add(mainLift4Fields);
+			
+			//accessory 1
+			accessory1Text.setText("Side Lateral");
+			mainPanel.add(accessory1Text);
+			
+			mainPanel.add(accessory1Fields);
+			
+			//accessory 2
+			accessory2Text.setText("Read Delt Fly");
+			mainPanel.add(accessory2Text);
+			
+			mainPanel.add(accessory2Fields);
+			
+			//accessory 3
+			accessory3Text.setText("Cable Fly");
+			mainPanel.add(accessory3Text);
+			
+			mainPanel.add(accessory3Fields);
+		}
+		else if (input.equals("Workout 4")) {
+			//main lift 1
+			mainLift1Text.setText("Bench Press");
+			mainPanel.add(mainLift1Text);
+			
+			mainPanel.add(mainLift1Fields);
+			
+			//main lift 2
+			mainLift2Text.setText("Deadlift");
+			mainPanel.add(mainLift2Text);
+			
+			mainPanel.add(mainLift2Fields);
+			
+			//main lift 3
+			mainLift3Text.setText("Overhead Press");
+			mainPanel.add(mainLift3Text);
+			
+			mainPanel.add(mainLift3Fields);
+			
+			//main lift 4
+			mainLift4Text.setText("Squat");
+			mainPanel.add(mainLift4Text);
+			
+			mainPanel.add(mainLift4Fields);
+			
+			//accessory 1
+			accessory1Text.setText("Lat Pulldown");
+			mainPanel.add(accessory1Text);
+			
+			mainPanel.add(accessory1Fields);
+			
+			//accessory 2
+			accessory2Text.setText("Tricep Pushdown");
+			mainPanel.add(accessory2Text);
+			
+			mainPanel.add(accessory2Fields);
+			
+			//accessory 3
+			accessory3Text.setText("Calf Raise");
+			mainPanel.add(accessory3Text);
+			
+			mainPanel.add(accessory3Fields);
+		}
+			
 		JLabel buttons = new JLabel();
 		buttons.setLayout(new FlowLayout());
 		
@@ -359,13 +495,13 @@ public class TrackAWorkout {
 				
 				data.add(dateInput.getText());
 				
-				if (workoutNumber == 1)
+				if (input.equals("Workout 1"))
 					data.add("Workout 1");
-				else if (workoutNumber == 2)
+				else if (input.equals("Workout 2"))
 					data.add("Workout 2");
-				else if (workoutNumber == 3)
+				else if (input.equals("Workout 3"))
 					data.add("Workout 3");
-				else if (workoutNumber == 4)
+				else if (input.equals("Workout 4"))
 					data.add("Workout 4");
 				
 				data.add(mainLift1Text.getText());
